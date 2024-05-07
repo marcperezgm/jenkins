5ffe2f37ffa886256d6240ce315b083f48064e10
marcperezgm <perezgm@ibadia.cat>
2024-05-08 01:50:10 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,3 @@
-
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
@@ -6,4 +5,3 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hola Mundo");
   }
-}
